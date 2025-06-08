b1ab66872cb314fec90678b310f9cec2e2e72d55
ProductOfAmerica <jeff.smith34r32@gmail.com>
2025-06-08 18:41:21 -0500

    Quick fix to starter page 2

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -10,11 +10,7 @@ type Props = Omit<ImageProps, 'src'> & {
 const ThemeImage = (props: Props) => {
 	const { srcLight, srcDark, ...rest } = props;
 
-	return (
-		<>
-			<Image {...rest} src={srcLight} className="imgLight" />
-		</>
-	);
+	return <Image {...rest} src={srcLight} className="imgLight" />;
 };
 
 export default function Home() {
